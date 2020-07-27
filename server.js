b4457fc991cdf9919e8fbc8c4b8a37d29154caa4
Torstein Skarsgard <torstein.skarsgard@gmail.com>
2020-07-27 14:54:54 +0200

    initial commit

--- server.js
+++ server.js
@@ -11,3 +11,4 @@ app.use(express.static(path.join(__dirname, "build")));
 app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, "build", "index.html")));
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
+ 
